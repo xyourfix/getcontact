31dc05d7220887c3e354c1b9d3c9164c015cb108
xyourfix <98245853+xyourfix@users.noreply.github.com>
2025-07-15 03:06:33 +0700

    Fix PostCSS config and Express version

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,9 @@
-import tailwindcss from '@tailwindcss/postcss'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 export default {
-  plugins: {
-    [tailwindcss]: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    tailwindcss(),
+    autoprefixer(),
+  ],
 }

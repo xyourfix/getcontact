aa44898fe242ac3fb578a427a0a936af8a525074
xyourfix <98245853+xyourfix@users.noreply.github.com>
2025-07-15 03:22:28 +0700

    Updated postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
-import tailwindcss from 'tailwindcss'
+import tailwindCssPlugin from '@tailwindcss/postcss'
 import autoprefixer from 'autoprefixer'
 
 export default {
   plugins: [
-    tailwindcss(),
+    tailwindCssPlugin(),
     autoprefixer(),
   ],
 }
